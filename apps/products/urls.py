34b8d1785b0b689af2d1813c44341d143f9d4b96
Alexander <alexander.rojas@ucuenca.edu.ec>
2025-06-14 16:45:16 -0500

    Integración de DRF y ORM para productos

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hola/', views.hola_mundo, name='hola'),
+    path('products/', views.product_list, name='product-list'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
 ]
